5fb6b1b7de463a429752ff0b0dd47d28a27b569b
CarlosRocha-98 <85251641+CarlosRocha-98@users.noreply.github.com>
2024-02-06 20:58:29 -0300

    adicionado pastas

---
+++ assets/script/Owl/main.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 30,
+    nav: false,
+    responsive: {
+        0: {
+            items: 2.5,
+            margin:12
+        },
+        600: {
+            items: 3.5
+        },
+        1000: {
+            items: 5.7
+        }
+    }
+})
